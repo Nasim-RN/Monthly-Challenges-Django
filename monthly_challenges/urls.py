67cc035a8098768f9123f2dd7d5a8b1bc39ff311
Nasim R.Niyar <n.rouhollahi@gmail.com>
2023-11-19 16:25:57 +0100

    Configure main urls.py for the 'monthly_challenges' app

--- monthly_challenges/urls.py
+++ monthly_challenges/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Route to the Django admin interface
     path('admin/', admin.site.urls),
+
+    # Include URL patterns from the 'challenges' app when the path is '/challenges/'
+    path("challenges/", include("challenges.urls"))
 ]
